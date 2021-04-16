6e9b3c950ce5c9b9691580e351c85817872663d1
unknown <microa4@gmail.com>
2021-04-17 01:51:40 +0200

    retoques y efectps formulariofdhs

--- script.js
+++ script.js
@@ -440,24 +440,24 @@ function validarFormulario() {
     return false;
   }
 
-  // var response = grecaptcha.getResponse();
-  // if (response.length == 0) {
-  //   //   alert("Captcha no verificado");
-  //   addClassNo();
-  //   $(recaptcha).addClass("form-error");
-  //   // $(recaptcha).addClass("color-error");
-  //   setTimeout(function () {
-  //     //   alert("Escriba su recaptcha por favor.");
-  //   }, 1000);
-  //   setTimeout(function () {
-  //     $(recaptcha).removeClass("form-error");
-  //   }, 1000);
-  //   recaptcha.focus();
-  //   verificar = false;
-  //   return false;
-  // } else {
-  //   // alert("Captcha verificado");
-  // }
+  var response = grecaptcha.getResponse();
+  if (response.length == 0) {
+    //   alert("Captcha no verificado");
+    addClassNo();
+    $(recaptcha).addClass("form-error");
+    // $(recaptcha).addClass("color-error");
+    setTimeout(function () {
+      //   alert("Escriba su recaptcha por favor.");
+    }, 1000);
+    setTimeout(function () {
+      $(recaptcha).removeClass("form-error");
+    }, 1000);
+    recaptcha.focus();
+    verificar = false;
+    return false;
+  } else {
+    // alert("Captcha verificado");
+  }
 
   if (verificar == true) {
     toggleClass();
